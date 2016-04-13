a94db7873c76deb21d1b42b895bb8f3c20cff70f
Brandon <btoney434@gmail.com>
2016-04-13 10:38:59 -0400

    Upadated Contact Seeder
    seeds 10 times as opposed to once.

--- database/seeds/ContactsTableSeeder.php
+++ database/seeds/ContactsTableSeeder.php
@@ -12,11 +12,13 @@ class ContactsTableSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('contacts')->insert([
-            'name' => str_random(10),
-            'phone' => rand(1000000000,9999999999),
-            'email' => str_random(10).'@host.com',
-        ]);
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('contacts')->insert([
+                'name' => str_random(10),
+                'phone' => rand(1000000000,9999999999),
+                'email' => str_random(10).'@host.com',
+            ]);
+        }
         $this->command->info('Done');
     }
 }
